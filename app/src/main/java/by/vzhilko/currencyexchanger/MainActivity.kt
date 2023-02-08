2aae6b30d4226856d2bc12913657bd69eaf67633
Valentin Zhilko <>
2023-02-08 17:22:19 +0100

    First penitration)

---
+++ app/src/main/java/by/vzhilko/currencyexchanger/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.vzhilko.currencyexchanger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
